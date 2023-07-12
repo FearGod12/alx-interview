e8338632b68fa3bba6302a5aca59634ffb49227e
Onyenike Chukwudi FearGod <onyenikechukwudi@gmail.com>
2023-07-12 23:33:42 +0100

    another method

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
-"""Minimum operations"""
-
-
-def isprime(n):
-    """check if its a prime number"""
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
+"""Minimum operations using the prime_factors method"""
 
 
 def minOperations(n):
-    """minimum operations"""
-    if not isinstance(n, int) or n <= 1:
-        return 0
-    file = 'H'
-    count = 0
-    copyall = 'H'
-    for i in range(1, n):
-        if n % i == 0 and isprime(i):
-            if len(file + copyall) <= n:
-                count += 1
-                copyall = file[:]
-                count += 1
-                file += copyall
-        else:
-            if len(file + copyall) <= n:
-                file += copyall
-                count += 1
-    return count
+    factors = []
+
+    # Divide by 2 repeatedly until it is no longer divisible by 2
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+
+    # Divide by odd numbers starting from 3 up to the square root of n
+    # Since any remaining factor must be prime
+    for i in range(3, int(n**0.5) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+
+    # If n is still greater than 2, it is a prime factor itself
+    if n > 2:
+        factors.append(n)
+
+    return sum(factors)
